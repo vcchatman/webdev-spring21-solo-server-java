98a3243a6fd416f26a9415166966caf9f7495c21
Vanessa Chatman <chatman.v@northeastern.edu>
2021-03-24 20:23:32 -0700

    A5 - incomplete

--- src/main/java/com/example/wbdvsp2103vcchatmanserverjava/controllers/WidgetController.java
+++ src/main/java/com/example/wbdvsp2103vcchatmanserverjava/controllers/WidgetController.java
@@ -1,22 +1,52 @@
+// Controller is responsible for all things HTTP, not responsible for data
+// The service is responsible for the data, knows nothing of HTTP
+
 package com.example.wbdvsp2103vcchatmanserverjava.controllers;
 
 import com.example.wbdvsp2103vcchatmanserverjava.models.Widget;
 import com.example.wbdvsp2103vcchatmanserverjava.services.WidgetService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController // this annotation makes all the content in this class accessible through HTTP
+@CrossOrigin(origins = "*")
 public class WidgetController {
 
     // dependency injection
     @Autowired
     WidgetService service;
 
-    @GetMapping("/find/all/widgets")
+    @PostMapping("/api/topics/{tid}/widgets")
+    public Widget createWidgetForTopic(
+            @PathVariable("tid") String topicId,
+            @RequestBody Widget widget
+    ) {
+        return service.createWidgetForTopic(topicId, widget);
+    }
+
+    @GetMapping("/api/widgets")
     public List<Widget> findAllWidgets() {
         return service.findAllWidgets();
     }
+
+    @GetMapping("/api/topics/{tid}/widgets")
+    public List<Widget> findWidgetsForTopic(@PathVariable("tid") String topicId) {
+        return service.findWidgetsForTopic(topicId);
+    }
+
+    @PutMapping("/api/widgets/{widgetId}")
+    public Integer updateWidget(
+            @PathVariable("widgetId") Long id,
+            @RequestBody Widget widget) {
+        return service.updateWidget(id, widget);
+    }
+
+    @DeleteMapping("/api/widgets/{widgetId}")
+    public Integer deleteWidget(
+            @PathVariable("widgetId") Long id) {
+        return service.deleteWidget(id);
+    }
+
 }
--- src/main/java/com/example/wbdvsp2103vcchatmanserverjava/services/WidgetService.java
+++ src/main/java/com/example/wbdvsp2103vcchatmanserverjava/services/WidgetService.java
@@ -1,13 +1,16 @@
 package com.example.wbdvsp2103vcchatmanserverjava.services;
+
 import com.example.wbdvsp2103vcchatmanserverjava.models.Widget;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class WidgetService {
     private List<Widget> widgets = new ArrayList<Widget>();
+
     {
         Widget w1 = new Widget(123l, "ABC123", "HEADING", 1, "Welcome to Widget for topic ABC123");
         Widget w2 = new Widget(234l, "ABC123", "PARAGRAPH", 1, "Dummy text.");
@@ -22,10 +25,58 @@ public class WidgetService {
         widgets.add(w5);
     }
 
+    public Widget createWidgetForTopic(String topicId, Widget widget) {
+        widget.setTopicId(topicId);
+        widget.setId((new Date()).getTime());
+        widgets.add(widget);
+        return widget;
+    }
+
     public List<Widget> findAllWidgets() {
         return widgets;
     }
 
+    public List<Widget> findWidgetsForTopic(String topicId) {
+        List<Widget> ws = new ArrayList<Widget>();
+        for (Widget w : widgets) {
+            if (w.getTopicId().equals(topicId)) {
+                ws.add(w);
+            }
+        }
+        return ws;
+    }
 
 
+    public Integer deleteWidget(Long id) {
+        int index = -1;
+        for (int i = 0; i < widgets.size(); i++) {
+            if (widgets.get(i).getId().equals(id)) {
+                index = i;
+                widgets.remove(index);
+                return 1;
+            }
+        }
+        return -1;
+    }
+
+    public Integer updateWidget(Long id, Widget widget) {
+        for (int i = 0; i < widgets.size(); i++) {
+            if (widgets.get(i).getId().equals(id)) {
+                widgets.set(i, widget);
+                return 1;
+            }
+        }
+        return -1;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
