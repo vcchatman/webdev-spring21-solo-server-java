2c44b39115e932e9be1c3f6a035f225859d48dc6
Vanessa Chatman <chatman.v@northeastern.edu>
2021-03-29 02:11:00 -0700

    Beginner database implemented.

--- src/main/java/com/example/wbdvsp2103vcchatmanserverjava/models/Widget.java
+++ src/main/java/com/example/wbdvsp2103vcchatmanserverjava/models/Widget.java
@@ -1,17 +1,30 @@
 package com.example.wbdvsp2103vcchatmanserverjava.models;
 
+import javax.persistence.*;
+
+// below annotates that we are intending to map this class to an equivalent table in  the database we have configured in our properties file
+@Entity
+@Table(name="widgets")
 public class Widget {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String topicId;
     private String type;
     private Integer size;
+    private Integer width;
+    private Integer height;
     private String text;
+    private String name;
 
-    public Widget(Long id, String topicId, String type, Integer size, String text) {
+    public Widget(Long id, String topicId, String type, Integer size, Integer width, Integer height, String text) {
         this.id = id;
         this.topicId = topicId;
         this.type = type;
         this.size = size;
+        this.width = width;
+        this.height = height;
         this.text = text;
     }
 
@@ -50,6 +63,21 @@ public class Widget {
         this.size = size;
     }
 
+    public Integer getWidth() {
+        return width;
+    }
+
+    public void setWidth(Integer width) {
+        this.width = width;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
     public String getText() {
         return text;
     }
@@ -57,4 +85,37 @@ public class Widget {
     public void setText(String text) {
         this.text = text;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
+
+//    create table widget (
+//        id bigint not null,
+//        height integer,
+//        size integer,
+//        text varchar(255),
+//    topic_id varchar(255),
+//    type varchar(255),
+//    width integer,
+//    primary key (id)
+//    ) engine=InnoDB
+
+//    create table widgets (
+//        id bigint not null auto_increment,
+//        height integer,
+//        size integer,
+//        text varchar(255),
+//    topic_id varchar(255),
+//    type varchar(255),
+//    width integer,
+//    primary key (id)
+//    ) engine=InnoDB
+
+//    alter table widgets
+//        add column name varchar(255)
--- src/main/java/com/example/wbdvsp2103vcchatmanserverjava/services/WidgetService.java
+++ src/main/java/com/example/wbdvsp2103vcchatmanserverjava/services/WidgetService.java
@@ -12,11 +12,11 @@ public class WidgetService {
     private List<Widget> widgets = new ArrayList<Widget>();
 
     {
-        Widget w1 = new Widget(123l, "ABC123", "HEADING", 1, "Welcome to Widget for topic ABC123");
-        Widget w2 = new Widget(234l, "ABC123", "PARAGRAPH", 1, "Dummy text.");
-        Widget w3 = new Widget(345l, "ABC234", "HEADING", 2, "Welcome to Widget for topic ABC234");
-        Widget w4 = new Widget(456l, "ABC234", "PARAGRAPH", 1, "Dummy text.");
-        Widget w5 = new Widget(567l, "ABC234", "PARAGRAPH", 1, "Dummy text.");
+        Widget w1 = new Widget(123l, "ABC123", "HEADING", 1, 1, 2, "Welcome to Widget for topic ABC123");
+        Widget w2 = new Widget(234l, "ABC123", "PARAGRAPH", 1, 2, 4, "Dummy text.");
+        Widget w3 = new Widget(345l, "ABC234", "HEADING", 2, 4, 8, "Welcome to Widget for topic ABC234");
+        Widget w4 = new Widget(456l, "ABC234", "PARAGRAPH", 1, 8, 16, "Dummy text.");
+        Widget w5 = new Widget(567l, "ABC234", "PARAGRAPH", 1, 16, 32, "Dummy text.");
 
         widgets.add(w1);
         widgets.add(w2);
